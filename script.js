0e8574723e0754900bb784320bd6411d0e4563b6
oleh Smolynets <smolynets@gmail.com>
2018-03-03 17:50:34 +0200

    add resize arrows under text

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
  
     function showingMenu(){
         $('.menu-items').toggleClass('hidden');
@@ -10,6 +12,54 @@ $(document).ready(function(){
 
     $('.menu-container').mouseout(function(){
         showingMenu();
+    })
+
+
+
+
+    function biggerArrow_left(){
+        $('.arrow_left').show().animate( {
+        	fontSize:"70px"
+        	} , 200 );
+    }
+
+    function lessArrow_left(){
+        $('.arrow_left').show().animate( {
+        	fontSize:"40px"
+        	} , 200 );
+    }
+
+
+    $('.arrow_left').mouseover(function(){
+        biggerArrow_left();
+    })
+
+    $('.arrow_left').mouseout(function(){
+        lessArrow_left();
+    })
+
+
+
+
+    function biggerArrow_right(){
+        $('.arrow_right').show().animate( {
+        	fontSize:"70px"
+        	} , 200 );
+    }
+
+    function lessArrow_right(){
+        $('.arrow_right').show().animate( {
+        	fontSize:"40px"
+        	} , 200 );
+    }
+
+
+    $('.arrow_right').mouseover(function(){
+        biggerArrow_right();
+    })
+
+    $('.arrow_right').mouseout(function(){
+        lessArrow_right();
     })
 
 });
